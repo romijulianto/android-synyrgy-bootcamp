c4e825085872769cd62e05fe12f9b9b2d60d00fc
Romi Julianto <romyjulians@gmail.com>
2022-10-28 23:15:27 +0700

    create recyclerview_abjad

---
+++ app/src/main/java/com/romijulianto/wordabjad/AbjadActivity.kt
@@ -0,0 +1,11 @@
+package com.romijulianto.wordabjad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AbjadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abjad)
+    }
+}
