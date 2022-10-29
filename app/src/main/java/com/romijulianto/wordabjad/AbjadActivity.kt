09bbb96b74284beca8b0336e659e5ddfb1a83a1b
Romi Julianto <romyjulians@gmail.com>
2022-10-29 11:36:56 +0700

    using linear layout manager

--- app/src/main/java/com/romijulianto/wordabjad/AbjadActivity.kt
+++ app/src/main/java/com/romijulianto/wordabjad/AbjadActivity.kt
@@ -3,13 +3,13 @@ package com.romijulianto.wordabjad
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class AbjadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_abjad)
-
 
         /* create list data */
         /*  val abjads = listOf<String>(
@@ -26,8 +26,16 @@ class AbjadActivity : AppCompatActivity() {
 
         /* if your application error, show Log.e */
         Log.e("AbjadActivity", "size ${abjads.size}")
+        Log.e("AbjadActivity", abjads[1])
+
+        abjads.forEach { abjad ->
+            Log.e("AbjadActivity", abjad)
+        }
 
         val abjadAdapter = AbjadAdapter( abjads )
-        findViewById<RecyclerView>(R.id.recyclerview_abjad).adapter = abjadAdapter
+        findViewById<RecyclerView>(R.id.recyclerview_abjad).apply {
+            layoutManager = LinearLayoutManager( this@AbjadActivity)
+            adapter = abjadAdapter
+        }
     }
 }
