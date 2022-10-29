c3ecca42fdb3662635e340d614242fb07afd68e0
Romi Julianto <68786238+romijulianto@users.noreply.github.com>
2022-10-30 01:21:17 +0700

    update option men linear to grid layout

--- app/src/main/java/com/romijulianto/wordabjad/activity/AbjadActivity.kt
+++ app/src/main/java/com/romijulianto/wordabjad/activity/AbjadActivity.kt
@@ -1,7 +1,11 @@
 package com.romijulianto.wordabjad.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.romijulianto.wordabjad.R
@@ -9,6 +13,8 @@ import com.romijulianto.wordabjad.adapter.AbjadAdapter
 import com.romijulianto.wordabjad.databinding.ActivityAbjadBinding
 
 class AbjadActivity : AppCompatActivity() {
+    /* track layout default */
+    private var isLinearLayoutManager = true
 
     /* initiate variable binding recyclerview */
     private lateinit var recyclerView: RecyclerView
@@ -22,8 +28,53 @@ class AbjadActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         recyclerView = binding.recyclerviewAbjad
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        // Sets the LinearLayoutManager of the recyclerview
+        chooseLayout()
+    }
+
+    /* create function and condition ChooseLayout */
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 2)
+        }
         recyclerView.adapter = AbjadAdapter()
-        
+    }
+
+    /* create function setIcon Boolean */
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_view)
+            else ContextCompat.getDrawable(this, R.drawable.ic_list_view)
+    }
+
+    /* create function onCreateOptionsmenu */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+
+        val layoutButton = menu?.findItem(R.id.it_switch_menu)
+        setIcon(layoutButton)
+        return true
+    }
+
+    /* create optionsMenu */
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.it_switch_menu -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
