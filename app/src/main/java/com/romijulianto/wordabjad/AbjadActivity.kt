58d6d83712aa6e965e4f82df6bb0699b1faba2fa
Romi Julianto <romyjulians@gmail.com>
2022-10-29 11:08:30 +0700

    revisi Abjad using for

--- app/src/main/java/com/romijulianto/wordabjad/AbjadActivity.kt
+++ app/src/main/java/com/romijulianto/wordabjad/AbjadActivity.kt
@@ -1,6 +1,7 @@
 package com.romijulianto.wordabjad
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,11 +12,20 @@ class AbjadActivity : AppCompatActivity() {
 
 
         /* create list data */
-        val abjads = listOf<String>(
+        /*  val abjads = listOf<String>(
             "A", "B", "C", "D", "E", "F", "G", "H", "I",
             "J", "K", "L", "M", "O", "P", "Q", "R", "S",
             "T", "U", "V", "W", "X", "Y", "Z",
-        )
+        )*/
+
+        val abjads = arrayListOf<String>()
+
+        for (item in 'A'..'Z'){
+            abjads.add(item.toString())
+        }
+
+        /* if your application error, show Log.e */
+        Log.e("AbjadActivity", "size ${abjads.size}")
 
         val abjadAdapter = AbjadAdapter( abjads )
         findViewById<RecyclerView>(R.id.recyclerview_abjad).adapter = abjadAdapter
